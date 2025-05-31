7afb5644a8469a382a0b71979ae1ee7b3665192b
sepideh <sepideh0771@gmail.com>
2025-05-31 02:20:52 -0700

    use state on input

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [age, setAge] = useState(0);
+  const [inputValue, setInputValue] = useState();
+  const handelChange=(e)=>{
+    setInputValue(e.target.value)
+  }
 
   return (
     <div className="App">
-      <h1>{age}</h1>
-      <button onClick={()=>setAge(age + 1)}>Increse age</button>
-      <button onClick={()=>setAge(age - 1)}>decrese age</button>
+      <input type="text" onChange={handelChange}></input>
+      <h1>{inputValue}</h1>
     </div>
   );
 }
-
 export default App;
