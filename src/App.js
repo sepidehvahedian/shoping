fd768f5a754dc41ad9f67d7a0f59e273ac0c443d
sepideh <sepideh0771@gmail.com>
2025-05-31 02:04:53 -0700

    increse and decrese age use of useState

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [age,setAge]=useState(0)
+  const [age, setAge] = useState(0);
 
   return (
     <div className="App">
       <h1>{age}</h1>
-      <button onClick={setAge(age+1)}>Increse age</button>
+      <button onClick={()=>setAge(age + 1)}>Increse age</button>
+      <button onClick={()=>setAge(age - 1)}>decrese age</button>
     </div>
   );
 }
