912733f2537e64a53bbf6199f460b8cd9200a51c
sepideh <sepideh0771@gmail.com>
2025-05-30 23:22:16 -0700

    style inline css

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import "./App.css";
 function App() {
   const age = 20;
 
-  if (age > 20) {
-    return <div className="App"> over age </div>;
-  } else return <div className="App"> under </div>;
+  return (
+    <div className={age ? "App" : "App-header"}>
+      {age > 20 ? <h1>over</h1> : <h1>under</h1>}
+      {age == 20 && <h1>age is 20</h1>}
+    </div>
+  );
 }
 
 export default App;
