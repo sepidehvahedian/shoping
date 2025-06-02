038701c962f747f768dadc34f484711e34c4c6c2
sepideh <sepideh0771@gmail.com>
2025-06-02 12:46:01 -0700

    axios api on input

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import "./App.css";
 import Axios from "axios";
 
 function App() {
-  const [catFact, setCatFact] = useState();
-
-  useEffect(() => {
-    fetchFact();
-  }, []);
+  const [name, setName] = useState("");
+  const [predicteResult, setPredictedResult] = useState({});
 
   const fetchFact = () => {
-    Axios.get("https://catfact.ninja/fact").then((res) => {
-      setCatFact(res.data.fact);
-    });
+    Axios.get(`https://api.agify.io?name=${text}`)
+      .then((response) => {
+        console.log("پاسخ سرور:", response.data);
+        setPredictedResult(response.data);
+      })
+      .catch((error) => {
+        console.error("خطا:", error);
+      });
   };
 
   return (
     <div className="App">
-      <button onClick={fetchFact}>fetch data</button>
-      <p>{catFact}</p>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <button onClick={fetchFact}>predicte age</button>
+      <h1>age is : {predicteResult.age}</h1>
+      <h1>age is : {predicteResult.name}</h1>
     </div>
   );
 }
