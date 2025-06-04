90c033f497eb6788bc31b5213d6e1a0ae012fa47
sepideh <sepideh0771@gmail.com>
2025-06-04 05:40:03 -0700

    react hook form

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import Axios from "axios";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Profile from "./pages/Profile";
-import { Nav } from "./pages/Nav";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SubmitForm from "./Components/SubmitForm";
 
 function App() {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <Router>
-          <div>SepidehStyle</div>
-          <Nav />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/profile/:name/:id?" element={<Profile />} />
-            <Route path="*" element={<div>Not Found</div>} />
-          </Routes>
-          <div>This is footer</div>
-        </Router>
-      </div>
-    </QueryClientProvider>
+    <div className="App">
+      <SubmitForm />
+    </div>
   );
 }
 
